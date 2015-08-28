a4a84eddc0aae4cd4c1f62cf6b00986fb736ed0f
Yusei Yamanaka <yusei1128@gmail.com>
2015-08-28 11:09:44 +0900

    Refactor

--- ether_sample_packet_tx.v
+++ ether_sample_packet_tx.v
@@ -2,7 +2,11 @@
 
 module ether_sample_packet_tx # (
   parameter TRUE  = 1'b1,
-  parameter FALSE = 1'b0
+  parameter FALSE = 1'b0,
+  parameter SRC_MAC_ADDR = { 8'h00, 8'h30, 8'h1b, 8'ha0, 8'ha4, 8'h8e },
+  parameter SRC_IP_ADDR  = { 8'd172, 8'd16, 8'd0, 8'd200 },
+  parameter DST_MAC_ADDR = { 8'hff, 8'hff, 8'hff, 8'hff, 8'hff, 8'hff },
+  parameter DST_IP_ADDR  = { 8'd172, 8'd16, 8'd0, 8'd1 }
 ) (
   input  wire       trig,
   input  wire       rst,
@@ -63,7 +67,7 @@ always @(posedge trig or posedge clk_125) begin
   if (trig) begin
     phy_en      <= 1'b0;
     phy_data    <= 8'b0;
-    crc_en      <= 1'b1;
+    crc_en      <= 1'b0;
     cnt         <= 12'd0;
   end
   else begin
@@ -72,56 +76,61 @@ always @(posedge trig or posedge clk_125) begin
         phy_en   <= 1'b1;
         phy_data <= 8'h55;
       end
-      12'h01: phy_data <= 8'h55;	// Preamble
+      12'h01: phy_data <= 8'h55;  // Preamble
       12'h02: phy_data <= 8'h55;
       12'h03: phy_data <= 8'h55;
       12'h04: phy_data <= 8'h55;
       12'h05: phy_data <= 8'h55;
       12'h06: phy_data <= 8'h55;
-      12'h07: phy_data <= 8'hd5;	// Preable + Start Frame Delimiter
-      12'h08: phy_data <= 8'hff;	// Destination MAC address = FF-FF-FF-FF-FF-FF-FF
+      12'h07: phy_data <= 8'hd5;  // Preable + Start Frame Delimiter
+      // Ethernet
+      12'h08: begin
+        phy_data <= 8'hff;  // Destination MAC address = FF-FF-FF-FF-FF-FF-FF
+        crc_en   <= 1'b1;
+      end
       12'h09: phy_data <= 8'hff;
       12'h0a: phy_data <= 8'hff;
       12'h0b: phy_data <= 8'hff;
       12'h0c: phy_data <= 8'hff;
       12'h0d: phy_data <= 8'hff;
-      12'h0e: phy_data <= 8'h00;	// Source MAC address = 00-30-1b-a0-a4-8e
-      12'h0f: phy_data <= 8'h30;
-      12'h10: phy_data <= 8'h1b;
-      12'h11: phy_data <= 8'ha0;
-      12'h12: phy_data <= 8'ha4;
-      12'h13: phy_data <= 8'h8e;
-      12'h14: phy_data <= 8'h08;	// Protocol Type = ARP (0x0806)
+      12'h0e: phy_data <= SRC_MAC_ADDR[47:40];  // Source MAC address = 00-30-1b-a0-a4-8e
+      12'h0f: phy_data <= SRC_MAC_ADDR[39:32];
+      12'h10: phy_data <= SRC_MAC_ADDR[31:24];
+      12'h11: phy_data <= SRC_MAC_ADDR[23:16];
+      12'h12: phy_data <= SRC_MAC_ADDR[15:8];
+      12'h13: phy_data <= SRC_MAC_ADDR[7:0];
+      12'h14: phy_data <= 8'h08;  // Protocol Type = ARP (0x0806)
       12'h15: phy_data <= 8'h06;
-      12'h16: phy_data <= 8'h00;	// Harware Type = Ethernet (1)
+      // IP Protocol
+      12'h16: phy_data <= 8'h00;  // Hardware Type = Ethernet (1)
       12'h17: phy_data <= 8'h01;
-      12'h18: phy_data <= 8'h08;	// Protocol Type = IP (0x0800)
+      12'h18: phy_data <= 8'h08;  // Protocol Type = IP (0x0800)
       12'h19: phy_data <= 8'h00;
-      12'h1a: phy_data <= 8'h06;	// Hardware size = 6
-      12'h1b: phy_data <= 8'h04;	// Protocol size = 4
-      12'h1c: phy_data <= 8'h00;	// Opcode = request (1)
+      12'h1a: phy_data <= 8'h06;  // Hardware size = 6
+      12'h1b: phy_data <= 8'h04;  // Protocol size = 4
+      12'h1c: phy_data <= 8'h00;  // Opcode = request (1)
       12'h1d: phy_data <= 8'h01;
-      12'h1e: phy_data <= 8'h00;	// Sender MAC address = 00-30-1b-a0-a4-8e
-      12'h1f: phy_data <= 8'h30;
-      12'h20: phy_data <= 8'h1b;
-      12'h21: phy_data <= 8'ha0;
-      12'h22: phy_data <= 8'ha4;
-      12'h23: phy_data <= 8'h8e;
-      12'h24: phy_data <= 8'd10;	// Sender IP address = 10.0.21.10
-      12'h25: phy_data <= 8'd0;
-      12'h26: phy_data <= 8'd21;
-      12'h27: phy_data <= 8'd10;
-      12'h28: phy_data <= 8'h00;	// Target MAC address = 00-00-00-00-00-00
+      12'h1e: phy_data <= SRC_MAC_ADDR[47:40];  // Sender MAC address = 00-30-1b-a0-a4-8e
+      12'h1f: phy_data <= SRC_MAC_ADDR[39:32];
+      12'h20: phy_data <= SRC_MAC_ADDR[31:24];
+      12'h21: phy_data <= SRC_MAC_ADDR[23:16];
+      12'h22: phy_data <= SRC_MAC_ADDR[15:8];
+      12'h23: phy_data <= SRC_MAC_ADDR[7:0];
+      12'h24: phy_data <= SRC_IP_ADDR[31:24];  // Sender IP address = 10.0.21.10
+      12'h25: phy_data <= SRC_IP_ADDR[23:16];
+      12'h26: phy_data <= SRC_IP_ADDR[15:8];
+      12'h27: phy_data <= SRC_IP_ADDR[7:0];
+      12'h28: phy_data <= 8'h00;  // Target MAC address = 00-00-00-00-00-00
       12'h29: phy_data <= 8'h00;
       12'h2a: phy_data <= 8'h00;
       12'h2b: phy_data <= 8'h00;
       12'h2c: phy_data <= 8'h00;
       12'h2d: phy_data <= 8'h00;
-      12'h2e: phy_data <= 8'd10;	// Target IP address = 10.0.21.99
-      12'h2f: phy_data <= 8'd0;
-      12'h30: phy_data <= 8'd21;
-      12'h31: phy_data <= 8'd99;
-      12'h32: phy_data <= 8'h00;	// Padding Area
+      12'h2e: phy_data <= DST_IP_ADDR[31:24];  // Target IP address = 10.0.21.99
+      12'h2f: phy_data <= DST_IP_ADDR[23:16];
+      12'h30: phy_data <= DST_IP_ADDR[15:8];
+      12'h31: phy_data <= DST_IP_ADDR[7:0];
+      12'h32: phy_data <= 8'h00;  // Padding Area
       12'h33: phy_data <= 8'h00;
       12'h34: phy_data <= 8'h00;
       12'h35: phy_data <= 8'h00;
@@ -149,11 +158,12 @@ always @(posedge trig or posedge clk_125) begin
       12'h48: begin
         phy_en   <= 1'b0;
         phy_data <= 8'h00;
-        crc_en   <= 1'b1;
       end
       default: phy_data <= 8'h0;
     endcase
-    cnt <= cnt + 12'd1;
+    if (cnt != 12'h48) begin
+      cnt <= cnt + 12'd1;
+    end
   end
 end
 
